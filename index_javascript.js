f7155a108f6a5644d41502b9fb9395debf029480
yongbeom <koyongbeom@gmail.com>
2021-07-23 23:48:49 +0900

    slider finished

--- index_javascript.js
+++ index_javascript.js
@@ -1,4 +1,32 @@
+
+var position = -10800;
+console.log(position);
+
+
+const sliderImage = document.querySelectorAll(".sliderimage");
+
+sliderImage.forEach(image => {
+    image.style.transform = `translateX(${position}px)`
+});
+
+
+
 const leftChevron = document.querySelector(".left_chevron");
-leftChevron.addEventListener("click", ()=>{
-    leftChevron.classList.add("active");
+
+leftChevron.addEventListener("click", (a)=>{
+    position = position+480;
+    console.log(position);
+    sliderImage.forEach(image => {
+        image.style.transform = `translateX(${position}px)`
+    });
+})
+
+const rightChevron = document.querySelector(".right_chevron");
+
+rightChevron.addEventListener("click", (a)=>{
+    position = position-480;
+    console.log(position);
+    sliderImage.forEach(image => {
+        image.style.transform = `translateX(${position}px)`
+    });
 })
